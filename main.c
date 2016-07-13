d4a84dadac2cd8d61378cbcee4a93f2b6fd3886a
stefan-misik <mail@stefanmisik.eu>
2016-07-13 18:28:24 +0200

    Added Y/N questions asking code to the basic tool code

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include "win_tile_manifest_gen.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /******************************************************************************/
 int static ask_yn_question(
     const char * question
 )
 {
-    return 0;
+    char answer;
+    
+    do
+    {
+        printf("%s", question);
+        fflush(stdout);
+        fflush(stdin);
+        
+        /* Get and decode the answer */
+        scanf("%c", &answer);
+        answer = tolower(answer);             
+    } 
+    while('y' != answer && 'n' != answer);
+    
+    return 'y' == answer;
 }
 
 /******************************************************************************/
@@ -27,19 +42,18 @@ int main(
     /* Get the tile color */
     printf("Enter tile color in the format RRR,GGG,BBB: ");
     fflush(stdout);
-    scanf("%hu,%hu,%hu", &c_r, &c_g, &c_b);
+    scanf("%hu,%hu,%hu", &c_r, &c_g, &c_b);    
     
     
-   
     /* Fill the tile configuration structure */
     tile.tile_color.red = (unsigned char)c_r;
     tile.tile_color.green = (unsigned char)c_g;
     tile.tile_color.blue = (unsigned char)c_b;
     tile.flags = 0;
-    tile.flags |= ask_yn_question("Show name on tile") * 
-            WIN_TILE_FLAGS_SHOW_NAME;
-    tile.flags |= ask_yn_question("Dark text on a tile") * 
-            WIN_TILE_FLAGS_FOREGROUND_DARK;
+    tile.flags |= ask_yn_question("Show name on tile? (y/n) ") ? 
+            WIN_TILE_FLAGS_SHOW_NAME : 0;
+    tile.flags |= ask_yn_question("Dark text on a tile? (y/n) ") ? 
+            WIN_TILE_FLAGS_FOREGROUND_DARK : 0;
     
     /* Generate manifest file */
     generate_win_tile_manifest(file_name, &tile);
